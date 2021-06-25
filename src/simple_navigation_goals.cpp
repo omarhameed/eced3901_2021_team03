cd9a351cc185883d1e4d05c3b23d1fbb88399fb5
Usman Kamran <us311212@dal.ca>
2021-06-25 01:58:32 -0300

    First attempt at a turn. Not fully functional yet.

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h> 
 #include <actionlib/client/simple_action_client.h>
+#include <math.h>
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 int main(int argc, char** argv){
     ros::init(argc, argv, "simple_navigation_goals");
@@ -11,15 +12,26 @@ int main(int argc, char** argv){
         ROS_INFO("Waiting for the move_base action server to come up"); 
         }
     move_base_msgs::MoveBaseGoal goal;
-    //we'll send a goal to the robot to move 1 meter forward 
+    // we'll send a goal to the robot to move 1.1 meter forward 
     goal.target_pose.header.frame_id = "base_link";
     goal.target_pose.header.stamp = ros::Time::now();
-    goal.target_pose.pose.position.x = 1.0; 
+    goal.target_pose.pose.position.x = 1.1; 
     goal.target_pose.pose.orientation.w = 1.0;
     ROS_INFO("Sending goal"); 
     ac.sendGoal(goal);
     ac.waitForResult(); 
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base moved 1 meter forward");
     else ROS_INFO("The base failed to move forward 1 meter for some reason");
+
+    //turn 90 degrees ccw
+    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.stamp = ros::Time::now();
+    goal.target_pose.pose.position.x = 0.0; 
+    goal.target_pose.pose.orientation.w = M_PI/2;
+    ROS_INFO("Sending goal"); 
+    ac.sendGoal(goal);
+    ac.waitForResult(); 
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base turned.");
+    else ROS_INFO("The base failed to turn for some reason");
 return 0; 
 }
