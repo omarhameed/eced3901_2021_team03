34448fccadb51940fb1e7fb90192acb855b268a6
omarhameed <84892442+omarhameed@users.noreply.github.com>
2021-06-25 17:10:59 -0300

    comments

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -5,28 +5,34 @@
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 int main(int argc, char** argv){
     ros::init(argc, argv, "simple_navigation_goals");
-    //tell the action client that we want to spin a thread by default 
+    /*tell the action client that we want to spin a thread by default which is comunicated 
+    through "move_base*/
     MoveBaseClient ac("move_base", true);
-    //wait for the action server to come up 
+    //wait for the action server to come up and is ready to begin processing goals
     while(!ac.waitForServer(ros::Duration(5.0))){
         ROS_INFO("Waiting for the move_base action server to come up"); 
         }
     move_base_msgs::MoveBaseGoal goal;
-    // we'll send a goal to the robot to move 1.1 meter forward 
+    // we'll send a goal to the robot to move 1.1 meter forward from the orgin (0.3048,0.3048)
     goal.target_pose.header.frame_id = "map"; //instead of base_link
     goal.target_pose.header.stamp = ros::Time::now();
+    /*Making the base move 1 meter forward in x-dirction and 0 in the y-dirction 
+    from the orgin (0.3048) in the "map" coordinate frame.*/
     goal.target_pose.pose.position.x = 1.4048; 
     goal.target_pose.pose.position.y = 0.3048; 
     goal.target_pose.pose.orientation.w = 1.0;
     ROS_INFO("Sending goal"); 
+    // Sending the goal to the move_base node for processing 
     ac.sendGoal(goal);
+    // Block until the move_base node is finished processing goal 
     ac.waitForResult(); 
+    // Checking if the goal is successful 
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base moved 1 meter forward");
     else ROS_INFO("The base failed to move forward 1 meter for some reason");
 
     move_base_msgs::MoveBaseGoal goal1;
-    //turn 90 degrees ccw
-    goal1.target_pose.header.frame_id = "map"; //instead of base_link
+    // turn 90 degrees ccw (x = 0.3048 + 1.1)(y = 0.3048 + 1.1 )
+    goal1.target_pose.header.frame_id = "map"; 
     goal1.target_pose.header.stamp = ros::Time::now();
     goal1.target_pose.pose.position.x = 1.4048; 
     goal1.target_pose.pose.position.y = 1.4048; 
@@ -39,8 +45,8 @@ int main(int argc, char** argv){
 
 
     move_base_msgs::MoveBaseGoal goal2;
-    //turn 90 degrees ccw
-    goal2.target_pose.header.frame_id = "map"; //instead of base_link
+    //turn 90 degrees ccw (x = 0.3048 )(y = 0.3048 + 1.1 )
+    goal2.target_pose.header.frame_id = "map"; 
     goal2.target_pose.header.stamp = ros::Time::now();
     goal2.target_pose.pose.position.x = 0.3048; 
     goal2.target_pose.pose.position.y = 1.4048; 
@@ -52,8 +58,8 @@ int main(int argc, char** argv){
     else ROS_INFO("The base failed to turn for some reason");
 
     move_base_msgs::MoveBaseGoal goal3;
-    //turn 90 degrees ccw
-    goal3.target_pose.header.frame_id = "map"; //instead of base_link
+    //turn 90 degrees ccw (x = 0.3048)(y = 0.3048)
+    goal3.target_pose.header.frame_id = "map"; 
     goal3.target_pose.header.stamp = ros::Time::now();
     goal3.target_pose.pose.position.x = 0.3048; 
     goal3.target_pose.pose.position.y = 0.3048; 
@@ -64,17 +70,6 @@ int main(int argc, char** argv){
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base turned.");
     else ROS_INFO("The base failed to turn for some reason");
 
-    // move_base_msgs::MoveBaseGoal goal4;
-    // //turn 90 degrees ccw
-    // goal4.target_pose.header.frame_id = "base_link"; //instead of base_link
-    // goal4.target_pose.header.stamp = ros::Time::now();
-    // goal4.target_pose.pose.position.x = 1.1; 
-    // // goal1.target_pose.pose.position.y = -1.1; 
-    // goal4.target_pose.pose.orientation.w = 1;
-    // ROS_INFO("Sending goal"); 
-    // ac.sendGoal(goal4);
-    // ac.waitForResult(); 
-    // if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base turned.");
-    // else ROS_INFO("The base failed to turn for some reason");
+
 return 0; 
 }
