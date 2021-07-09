f1b8f293ab35cf6ca31bf6a8b151b76ff3f3327d
Usman Kamran <us311212@dal.ca>
2021-07-09 01:03:52 -0300

    First pass at navigating through the map. 4 waypoints set at fires, 1 waypoint at exit.

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -7,69 +7,83 @@ int main(int argc, char** argv){
     ros::init(argc, argv, "simple_navigation_goals");
     /*tell the action client that we want to spin a thread by default which is comunicated 
     through "move_base*/
+    float foot = 0.3048;
     MoveBaseClient ac("move_base", true);
     //wait for the action server to come up and is ready to begin processing goals
     while(!ac.waitForServer(ros::Duration(5.0))){
         ROS_INFO("Waiting for the move_base action server to come up"); 
         }
-    move_base_msgs::MoveBaseGoal goal;
-    // we'll send a goal to the robot to move 1.1 meter forward from the orgin (0.3048,0.3048)
-    goal.target_pose.header.frame_id = "map"; //instead of base_link
-    goal.target_pose.header.stamp = ros::Time::now();
+    move_base_msgs::MoveBaseGoal fire1;
+    // we'll send a fire1 to the robot to move 1.1 meter forward from the orgin (0.3048,0.3048)
+    fire1.target_pose.header.frame_id = "map"; //instead of base_link
+    fire1.target_pose.header.stamp = ros::Time::now();
     /*Making the base move 1 meter forward in x-dirction and 0 in the y-dirction 
     from the orgin (0.3048) in the "map" coordinate frame.*/
-    goal.target_pose.pose.position.x = 1.4048; 
-    goal.target_pose.pose.position.y = 0.3048; 
-    goal.target_pose.pose.orientation.w = 1.0;
-    ROS_INFO("Sending goal"); 
-    // Sending the goal to the move_base node for processing 
-    ac.sendGoal(goal);
+    fire1.target_pose.pose.position.x = 7*foot; 
+    fire1.target_pose.pose.position.y = foot; 
+    fire1.target_pose.pose.orientation.w = 1.0;
+    ROS_INFO("Sending fire1"); 
+    // Sending the fire1 to the move_base node for processing 
+    ac.sendGoal(fire1);
     // Block until the move_base node is finished processing goal 
     ac.waitForResult(); 
     // Checking if the goal is successful 
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base moved 1 meter forward");
     else ROS_INFO("The base failed to move forward 1 meter for some reason");
 
-    move_base_msgs::MoveBaseGoal goal1;
+    move_base_msgs::MoveBaseGoal fire2;
     // turn 90 degrees ccw (x = 0.3048 + 1.1)(y = 0.3048 + 1.1 )
-    goal1.target_pose.header.frame_id = "map"; 
-    goal1.target_pose.header.stamp = ros::Time::now();
-    goal1.target_pose.pose.position.x = 1.4048; 
-    goal1.target_pose.pose.position.y = 1.4048; 
-    goal1.target_pose.pose.orientation.w = 1;
+    fire2.target_pose.header.frame_id = "map"; 
+    fire2.target_pose.header.stamp = ros::Time::now();
+    fire2.target_pose.pose.position.x = 3*foot; 
+    fire2.target_pose.pose.position.y = 5*foot; 
+    fire2.target_pose.pose.orientation.w = 1;
     ROS_INFO("Sending goal"); 
-    ac.sendGoal(goal1);
+    ac.sendGoal(fire2);
     ac.waitForResult(); 
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base turned.");
     else ROS_INFO("The base failed to turn for some reason");
 
 
-    move_base_msgs::MoveBaseGoal goal2;
+    move_base_msgs::MoveBaseGoal fire3;
     //turn 90 degrees ccw (x = 0.3048 )(y = 0.3048 + 1.1 )
-    goal2.target_pose.header.frame_id = "map"; 
-    goal2.target_pose.header.stamp = ros::Time::now();
-    goal2.target_pose.pose.position.x = 0.3048; 
-    goal2.target_pose.pose.position.y = 1.4048; 
-    goal2.target_pose.pose.orientation.w = 1;
+    fire3.target_pose.header.frame_id = "map"; 
+    fire3.target_pose.header.stamp = ros::Time::now();
+    fire3.target_pose.pose.position.x = 11*foot; 
+    fire3.target_pose.pose.position.y = 11*foot; 
+    fire3.target_pose.pose.orientation.w = 1;
     ROS_INFO("Sending goal"); 
-    ac.sendGoal(goal2);
+    ac.sendGoal(fire3);
     ac.waitForResult(); 
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base turned.");
     else ROS_INFO("The base failed to turn for some reason");
 
-    move_base_msgs::MoveBaseGoal goal3;
+    move_base_msgs::MoveBaseGoal fire4;
     //turn 90 degrees ccw (x = 0.3048)(y = 0.3048)
-    goal3.target_pose.header.frame_id = "map"; 
-    goal3.target_pose.header.stamp = ros::Time::now();
-    goal3.target_pose.pose.position.x = 0.3048; 
-    goal3.target_pose.pose.position.y = 0.3048; 
-    goal3.target_pose.pose.orientation.w = 1;
+    fire4.target_pose.header.frame_id = "map"; 
+    fire4.target_pose.header.stamp = ros::Time::now();
+    fire4.target_pose.pose.position.x =7*foot; 
+    fire4.target_pose.pose.position.y = 11*foot; 
+    fire4.target_pose.pose.orientation.w = 1;
     ROS_INFO("Sending goal"); 
-    ac.sendGoal(goal3);
+    ac.sendGoal(fire4);
     ac.waitForResult(); 
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base turned.");
     else ROS_INFO("The base failed to turn for some reason");
 
-
-return 0; 
+    move_base_msgs::MoveBaseGoal exit;
+    //turn 90 degrees ccw (x = 0.3048)(y = 0.3048)
+    exit.target_pose.header.frame_id = "map"; 
+    exit.target_pose.header.stamp = ros::Time::now();
+    exit.target_pose.pose.position.x =13*foot; 
+    exit.target_pose.pose.position.y = 1*foot; 
+    exit.target_pose.pose.orientation.w = 1;
+    ROS_INFO("Sending goal"); 
+    ac.sendGoal(exit);
+    ac.waitForResult(); 
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base turned.");
+    else ROS_INFO("The base failed to turn for some reason");
+    
+    
+    return 0; 
 }
